8c4cccc22dc436e7c03e0fb4ead3694bfe5414e0
JorgeBanon <jbanver@gmail.com>
2022-07-29 20:15:40 +0200

    Initial commit

---
+++ ejercicios-tema-3.java
@@ -0,0 +1,20 @@
+// Suma de tres números
+
+public static int suma(int a, int b, int c) 
+{
+    int resultado = a + b + c;
+    return resultado;
+}
+
+suma();
+
+// Clase Coche
+
+class miCoche {
+    private int Puertas = 3;
+
+    public void addPuerta() {
+        Puertas = Puertas++;
+    }
+}
+System.out.println(miCoche.Puertas);
